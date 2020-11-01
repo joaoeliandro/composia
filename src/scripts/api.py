495c1322f20b47180773719bfb5f2765b2a60b7f
nulgas <nulgas@gmail.com>
2020-11-01 20:00:10 -0300

    integração da API com modelo

--- src/scripts/api.py
+++ src/scripts/api.py
@@ -1,12 +1,12 @@
-#pip install -q aitextgen
-
 import json
 from aitextgen import aitextgen
-from flask import Flask, request
+from flask import Flask, request, jsonify
+
 
 
 # importa o modelo
 ai = aitextgen(model="models/pytorch_model_luiz_gonzaga.bin", config="models/config_luiz_gonzaga.json")
+
 
 
 
@@ -16,12 +16,14 @@ app = Flask("composia")
 
 @app.route("/estrofe", methods=["GET"])
 def estrofe():
-    return jsonify({'text': ai.generate_one(batch_size=100,
+    text_raw = ai.generate_one(batch_size=100,
                         prompt=request.args['input'],
                         max_length=100,
                         temperature=1.5,
                         top_k=0,
                         top_p=1)
-                      })
+                      
+    text_clean = {'estrofe': text_raw.split('\n')[:-1]}
+    return text_clean
 
 app.run()
